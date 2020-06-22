d33f896741eab67cb0a589ccde3239b7c22ef362
Morita_Dai <moritadai9274@gmail.com>
2020-06-22 10:02:37 +0900

    start!

--- Assets/hoge.cs
+++ Assets/hoge.cs
@@ -7,7 +7,7 @@ public class hoge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ReSTARTだぁ！マジ卍！");
     }
 
     // Update is called once per frame
